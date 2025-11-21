1f5caaa116e6d221e89fedb0e898a44cc257ff88
TheMorrisino <m.spam@gmx.ch>
2025-11-21 20:00:11 +0100

    Ordner TOOLchain angepasst und Morris Odner mit einem File hinzugefügt

--- 1_Semesterarbeit/Ressourcix/src/Employee.kt
+++ 1_Semesterarbeit/Ressourcix/src/Employee.kt
@@ -2,9 +2,9 @@ data class Employee(
     var Id : UInt,
     var Role : UInt,
     var workloadPercentage : UByte,
-    var Tags: List<UByte> = List(30) { 0u }, //z.B Lehling im 1 Jahr, HF Asbildung, Anlehre, Diplomiert
+    var Tags: List<UByte> = List(30) { 0u },        //z.B Lehling im 1 Jahr, HF Asbildung, Anlehre, Diplomiert
     var Holidays : UByte,
-    var AbsentOnBusiness : Boolean , //nicht im Geschäfft
+    var AbsentOnBusiness : Boolean,                      //nicht im Geschäfft
     var Pedro : String
 
 )
