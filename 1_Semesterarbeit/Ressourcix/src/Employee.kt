8553adc448c41d17b9ee895ed957bf1525a4c426
TheMorrisino <m.spam@gmx.ch>
2025-11-28 11:13:51 +0100

    ID Zähler erstellt und in Varibale saveIdInListe abegspeichert

--- 1_Semesterarbeit/Ressourcix/src/Employee.kt
+++ 1_Semesterarbeit/Ressourcix/src/Employee.kt
@@ -1,10 +1,38 @@
-data class Employee(
-    var Id : UInt,
+import kotlin.UInt
+
+class Employee(
+    val Id: UInt = generateId(),
     var Role : UInt,
     var workloadPercentage : UByte,
-    var Tags: List<UByte> = List(30) { 0u },        //z.B Lehling im 1 Jahr, HF Asbildung, Anlehre, Diplomiert
+    var Tags: List<UByte> = List(30) { 0u },        //z.B Lehrling im 1 Jahr, HF Asbildung, Anlehre, Diplomiert
     var Holidays : UByte,
     var AbsentOnBusiness : Boolean,                      //nicht im Geschäfft
-    var Pedro : String
-
 )
+
+{
+
+
+    companion object {
+        private var IDcount : UInt = 0u
+        private var idliste = mutableListOf<UInt>()
+
+
+        private fun generateId() : UInt{
+            while (IDcount in idliste) {
+                IDcount += 1u
+
+
+            }
+            IDcount += 1u
+            val saveidinlist = (IDcount -1u)
+            idliste.add(saveidinlist)
+
+            println(idliste) //DEBUG
+
+            return saveidinlist
+
+
+        }
+    }
+}
+
---
+++ 1_Semesterarbeit/Ressourcix/src/main.kt
@@ -0,0 +1,39 @@
+fun main (){
+
+
+    var MeierMorris = Employee(Role = 10u, workloadPercentage = 80.toUByte(),
+        Holidays = 20.toUByte(), AbsentOnBusiness = false)
+
+    var DeSousaSaTiago = Employee(Role = 30u, workloadPercentage = 9000.toUByte(),
+        Holidays = 2.toUByte(), AbsentOnBusiness = false)
+
+    println(MeierMorris.Id)
+    println(DeSousaSaTiago.Id)
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
