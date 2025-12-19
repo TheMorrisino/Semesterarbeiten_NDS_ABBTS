f0b7ad4d20e9e0de783b4752daa9a4c7c5ec2137
Pedro Santos <ruysantos_41@icloud.com>
2025-12-19 16:03:51 +0100

    Merge von PS_FeaturesBranch

--- 1_Semesterarbeit/Ressourcix/src/Mitarbeiter.kt
+++ 1_Semesterarbeit/Ressourcix/src/Mitarbeiter.kt
@@ -1,7 +1,7 @@
 import kotlin.UInt
 
 class Employeeold(
-    val Id: UInt = generateId(),
+    val Id: UInt ,
     var Role : UInt,
     var workloadPercentage : UByte,
     var Tags: List<UByte> = List(30) { 0u },        //z.B Lehrling im 1 Jahr, HF Asbildung, Anlehre, Diplomiert
